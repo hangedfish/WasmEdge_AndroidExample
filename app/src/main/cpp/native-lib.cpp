62401123737eec4cdb22f1f929f72c32ccdf19cf
HangedFish <bravohangedman@outlook.com>
2022-01-28 18:53:38 +0800

    add fibonacci demo
    Signed-off-by: HangedFish <bravohangedman@outlook.com>

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,7 @@
 #include <jni.h>
 #include <string>
+#include <vector>
+#include <string>
 #include <wasmedge/wasmedge.h>
 
 
@@ -7,8 +9,39 @@ extern "C"
 JNIEXPORT
 jstring
 Java_com_hangedfish_wasmedge_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string str = std::string("WasmEdge Version: ") + WasmEdge_VersionGet();
     return env->NewStringUTF(str.c_str());
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_hangedfish_wasmedge_MainActivity_nativeWasmFib(JNIEnv *env, jobject thiz,
+                                                        jbyteArray j_wasm_bytes, jint idx) {
+    jsize n_wasm_bytes = env->GetArrayLength(j_wasm_bytes);
+    jbyte *wasm_bytes = env->GetByteArrayElements(j_wasm_bytes, 0);
+
+    WasmEdge_ConfigureContext *conf = WasmEdge_ConfigureCreate();
+    WasmEdge_ConfigureAddHostRegistration(conf, WasmEdge_HostRegistration_Wasi);
+
+    WasmEdge_VMContext *vm_ctx = WasmEdge_VMCreate(conf, nullptr);
+    WasmEdge_Value params[1] = {WasmEdge_ValueGenI32(idx)};
+    WasmEdge_Value retval[1]{};
+
+    const WasmEdge_String &func_name = WasmEdge_StringCreateByCString("fib");
+    const WasmEdge_Result &res = WasmEdge_VMRunWasmFromBuffer(vm_ctx, (uint8_t *) wasm_bytes,
+                                                              n_wasm_bytes,
+                                                              func_name, params, 1,
+                                                              retval, 1);
+    env->ReleaseByteArrayElements(j_wasm_bytes, wasm_bytes, 0);
+
+    WasmEdge_VMDelete(vm_ctx);
+    WasmEdge_ConfigureDelete(conf);
+    WasmEdge_StringDelete(func_name);
+
+    if (!WasmEdge_ResultOK(res)) {
+        return -1;
+    }
+    return WasmEdge_ValueGetI32(retval[0]);
 }
--- app/src/main/java/com/hangedfish/wasmedge/MainActivity.java
+++ app/src/main/java/com/hangedfish/wasmedge/MainActivity.java
@@ -2,10 +2,20 @@ package com.hangedfish.wasmedge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hangedfish.wasmedge.databinding.ActivityMainBinding;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +24,28 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("wasmedge_android_app");
     }
 
+    public native String stringFromJNI();
+
+    public native int nativeWasmFib(byte wasm_bytes[], int idx);
+
+
     private ActivityMainBinding binding;
+
+    private byte fib_wasm[];
+
+    private void readWasm() {
+        try {
+            fib_wasm = readAsset(this, "fibonacci.wasm");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        readWasm();
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -26,11 +53,61 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (fib_wasm == null) {
+                    Toast.makeText(MainActivity.this, "read wasm fail", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                int idx = 24;
+                int r = nativeWasmFib(fib_wasm, idx);
+                tv.setText(String.format("fib(%d) -> %d", idx, r));
+            }
+        });
     }
 
     /**
      * A native method that is implemented by the 'wasmedge' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+
+
+    public static byte[] readAsset(Context context, String filename)
+            throws IOException {
+        InputStream in = context.getResources().getAssets().open(filename);
+        try {
+            return readAllBytes(in);
+        } finally {
+            in.close();
+        }
+    }
+
+    public static byte[] readAllBytes(InputStream in) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        copyAllBytes(in, out);
+        return out.toByteArray();
+    }
+
+    /**
+     * Copies all available data from in to out without closing any stream.
+     *
+     * @return number of bytes copied
+     */
+    public static int copyAllBytes(InputStream in, OutputStream out)
+            throws IOException {
+        int byteCount = 0;
+        byte[] buffer = new byte[4096];
+        while (true) {
+            int read = in.read(buffer);
+            if (read == -1) {
+                break;
+            }
+            out.write(buffer, 0, read);
+            byteCount += read;
+        }
+        return byteCount;
+    }
+
 }
